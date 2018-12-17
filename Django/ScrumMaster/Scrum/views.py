1a6838d600f747ff518d8644d629262fa2bd5ca1
fasugba boluwatife <fasugbab@gmail.com>
2018-12-17 19:46:04 +0000

    Update views.py

--- Django/ScrumMaster/Scrum/views.py
+++ Django/ScrumMaster/Scrum/views.py
@@ -289,6 +289,7 @@ class ScrumGoalViewSet(viewsets.ModelViewSet):
         scrum_project_b = ScrumGoal.objects.get(id=request.data['goal_id'][1:]).user
         goal_id = request.data['goal_id'][1:]
         to_id = int(request.data['to_id'])
+        goal_item = ScrumGoal.objects.get(id=goal_id)
         
         if to_id == 4:
             if scrum_project_a.role == 'Developer':
@@ -301,7 +302,6 @@ class ScrumGoalViewSet(viewsets.ModelViewSet):
             self.createHistory(goal_item.name, goal_item.status, goal_item.goal_project_id, goal_item.hours, goal_item.time_created, goal_item.user, goal_item.project, goal_item.file, goal_item.id, 'Goal Removed Successfully!')
             return JsonResponse({'message': 'Goal Removed Successfully!', 'data': filtered_users(request.data['project_id'])})
         else:
-            goal_item = ScrumGoal.objects.get(id=goal_id)
             group = scrum_project_a.role
             from_allowed = []
             to_allowed = []
