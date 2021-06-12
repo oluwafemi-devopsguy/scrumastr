4ea4b9788874e0155a953859100dc1cd3fe471bc
git-fm <gfemi.ariyo@gmail.com>
2021-06-12 15:19:12 -0400

    Update hello_world.py

--- we_devops/hello_world.py
+++ we_devops/hello_world.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+FROM python:3
+ADD helloworld.py /
+RUN pip install flask
+RUN pip install flask_restful
+EXPOSE 3333
+CMD [ "python", "./helloworld.py"]
 
 print('Hello World!')
 print('This is my task which asks me to test any python app in a docker image')
